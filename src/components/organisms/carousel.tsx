bf11d830fdd71c2062bf90882e799841f068ed74
sadhu-sanjay <sanjaygoswami60@gmail.com>
2024-02-10 09:18:05 +0530

    Fixed the Slider

--- src/components/organisms/carousel.tsx
+++ src/components/organisms/carousel.tsx
@@ -4,8 +4,7 @@ import { ImagePlaceHolder } from "~/components/templates/image-placeholder";
 //@ts-expect-error i can't find the types for this
 import useSound from "use-sound";
 import hoverSound from "/hover.wav";
-import { NextButton } from "~/components/molecules/next-button"
-import { PrevButton } from "~/components/molecules/prev-button"
+import { ThumbNailSlider } from "~/components/organisms/thumbnail-slider"
 
 type ImageSliderProps = {
   images: any[];
@@ -92,7 +91,7 @@ const ImageSlider: React.FC<ImageSliderProps> = ({
   useEffect(() => {
     const interval = setInterval(() => {
       handleNextClick();
-    }, 30000);
+    }, 3000);
     return () => clearInterval(interval);
   }
   , [handleNextClick]);
@@ -104,66 +103,18 @@ const ImageSlider: React.FC<ImageSliderProps> = ({
         className={` w-full h-full bg-gray-900 relative`}
         data-carousel="slide"
       >
-        <div
-          onClick={() => {
-              setIsFullScreen(!isFullScreen);
-          }}
-          className="relative h-full w-full overflow-hidden bg-gray-100 dark:bg-gray-900"
-        >
-          {images &&
-            images?.map((item: any, index: number) => (
-              <div
-                key={index}
-                className={`duration-700 ease-in-out ${
-                  index === currentImageIndex ? "block" : "hidden"
-                }`}
-              data-carousel-item
-              >
-                <video autoPlay loop playsInline
-                  className={`absolute block w-full h-full transition-transform duration-700 ease-in-out transform object-cover
-                  -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2`}>
-                  <source 
-                  src={item.prev ? item.prev : item.image}
-                  type="video/webm" />
-                  Your browser does not support the video tag.
-                </video>
-
-              </div>
-            ))}
-          {!images && <ImagePlaceHolder />}
-        </div>
-
-        <div className={`${images?.length > 1 ? "block" : "hidden"}`}>
-          <div 
-          ref={gridRef}
-          className="absolute z-30 flex space-x-3 bottom-5  
-          grid-cols-1 grid-rows-1 gap-2 grid-auto-flow grid-auto-cols-[1fr] grid-auto-rows-[1fr] w-full h-auto px-4 py-4 bg-gray-900/10 dark:bg-gray-800/10 
-          overflow-hidden ">
-            {images?.map((item, index) => (
-              <div
-                key={index}
-                 // onFocus={play}
-                 tabIndex={index + 0}
-                style={{outline: "none", backgroundImage: `url(${item.image})`, backgroundSize: "cover"}}
-                 className={`focus:shadow-2xl focus:scale-110 focus:border-collapse 
-                 focus:shadow-amber-701 focus:border-amber-700 min-h-[131px] 
-                 min-w-[250px] shadow-lg  border rounded-xl relative 
-                 transform ease-in-out transition-transform duration-400
-                 `}
-                aria-current={index === currentImageIndex}
-                aria-label={`Slide ${index + 1}`}
-                data-carousel-slide-to={index}
-                onClick={(e) => {
-                  e.stopPropagation();
-                  setCurrentImageIndex(index);
-                }}
-              >
-              </div>
-            ))}
-          </div>
-            <PrevButton onClick={handlePrevClick} /> 
-          <NextButton onClick={handleNextClick} /> 
-        </div>
+        <SlideShow
+            items={images}
+            setIsFullScreen={setIsFullScreen}
+            currentImageIndex={currentImageIndex}
+            />
+        <ThumbNailSlider 
+            componentRef={gridRef}
+            images={images}
+            selectedIndex={currentImageIndex}
+            onClick={(index) => setCurrentImageIndex(index)}
+        />            
+         
       </div>
     </>
   );
@@ -171,3 +122,37 @@ const ImageSlider: React.FC<ImageSliderProps> = ({
 
 export default ImageSlider;
 
+type SlideShowProps = {
+    setIsFullScreen: (flag: boolean) => void;
+    items: Array<any>;
+    currentImageIndex: number;
+}
+
+const SlideShow: React.FC<SlideShowProps> = ({setIsFullScreen, items, currentImageIndex}) => {
+
+return (
+    <div
+          onClick={() => {
+              setIsFullScreen(!isFullScreen);
+          }}
+          className="relative h-full w-full overflow-hidden bg-gray-100 dark:bg-gray-900"
+        >
+          {items &&
+            items?.map((item: any, index: number) => (
+              <div
+                key={index}
+                className={`duration-700 ease-in-out ${
+                  index === currentImageIndex ? "block" : "hidden"
+                }`}
+              data-carousel-item
+              >
+                <img 
+                    src="/vite.svg"
+                    className={"w-full h-full"}
+                    />
+
+              </div>
+            ))}
+          {!items && <ImagePlaceHolder />}
+        </div>)
+}
---
+++ src/components/organisms/thumbnail-slider.tsx
@@ -0,0 +1,42 @@
+
+type SliderProps = {
+    componentRef: any;
+    images: Array<any>;
+    currentImageIndex: number;
+    onSelect: (index: number) => void
+}
+const ThumbNailSlider: React.FC<SliderProps> = ({componentRef, images, currentImageIndex, onSelect}) => {
+    
+    return (<div className={``}>
+          <div 
+          ref={componentRef}
+          className="absolute z-30 flex space-x-3 bottom-5  
+          grid-cols-1 grid-rows-1 gap-2 grid-auto-flow grid-auto-cols-[1fr] grid-auto-rows-[1fr] w-full h-auto px-4 py-4 bg-gray-900/10 dark:bg-gray-800/10 
+          overflow-hidden ">
+            {images?.map((item, index) => (
+              <div
+                key={index}
+                 // onFocus={play}
+                 tabIndex={index + 0}
+                style={{outline: "none", backgroundImage: `url(${item.image})`, backgroundSize: "cover"}}
+                 className={`focus:shadow-2xl focus:scale-110 focus:border-collapse 
+                 focus:shadow-amber-701 focus:border-amber-700 min-h-[131px] 
+                 min-w-[250px] shadow-lg  border rounded-xl relative 
+                 transform ease-in-out transition-transform duration-400
+                 `}
+                aria-current={index === currentImageIndex}
+                aria-label={`Slide ${index + 1}`}
+                data-carousel-slide-to={index}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  onClick(index)
+                }}
+              >
+              </div>
+            ))}
+          </div>
+        </div>)
+
+}
+
+export { ThumbNailSlider }
